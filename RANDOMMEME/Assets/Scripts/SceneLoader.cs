e31166325b5e236e42cb93b501ace0fdeb8dd524
Semen <35036074+Iridium6games@users.noreply.github.com>
2019-07-08 23:15:59 +0500

    +++SceneManager

---
+++ RANDOMMEME/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   
+    public void LoadGamePlay()
+    {
+        Application.LoadLevel("Gameplay");
+    }
+    public void LoadSettings()
+    {
+        Application.LoadLevel("Settings");
+    }
+    public void LoadMore()
+    {
+        Application.LoadLevel("More");
+    }
+    public void LoadUpMem()
+    {
+        Application.LoadLevel("UpMem");
+    }
+    public void LoadDownMem()
+    {
+        Application.LoadLevel("DownMem");
+    }
+    public void LoadMainMenu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+}
