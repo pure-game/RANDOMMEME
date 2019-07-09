c0a5bcde6f8444d9608841b774cf68f5f5cada51
Semen <35036074+Iridium6games@users.noreply.github.com>
2019-07-09 18:28:21 +0500

    +++ADS

--- RANDOMMEME/Assets/Scripts/GamePlay.cs
+++ RANDOMMEME/Assets/Scripts/GamePlay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using GoogleMobileAds.Api;
 
 public class GamePlay : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class GamePlay : MonoBehaviour
     Image memeImage;
     public TextMeshProUGUI upMeme;
     public TextMeshProUGUI downMeme;
+    public int adsBuy = 0;
+    public const string bannerID = "ca-app-pub-7272958162561065/4152458345";
+    public const string adID = "ca-app-pub-7272958162561065/4152458345";
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,18 @@ public class GamePlay : MonoBehaviour
 
     public void ButtonClick()
     {
+       /* if(adsBuy == 0)
+        {
+            BannerView bannerView = new BannerView(bannerID, AdSize.Banner, AdPosition.Bottom);
+            AdRequest adRequest = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice("320D89DA65836B55").Build();
+            bannerView.LoadAd(adRequest);
+
+            InterstitialAd interstitialAd = new InterstitialAd(adID);
+            AdRequest ad = new AdRequest.Builder().AddTestDevice(AdRequest.TestDeviceSimulator).AddTestDevice("320D89DA65836B55").Build();
+            interstitialAd.LoadAd(ad);
+            if (interstitialAd.IsLoaded()) interstitialAd.Show();
+        }*/
+
         memeImage.sprite = Resources.Load<Sprite>("meme_" + Random.Range(1, 50));
         upMeme.text = MainMenu.strings_up[Random.Range(0, MainMenu.strings_up.Count)];
         downMeme.text = MainMenu.strings_down[Random.Range(0, MainMenu.strings_down.Count)];
