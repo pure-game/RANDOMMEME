74c74e9aa80b1e9a96ce7ec6744e5276e7af0b7d
Semen <35036074+Iridium6games@users.noreply.github.com>
2019-07-12 03:06:34 +0500

    +++DeletingMemes(Bugs)

---
+++ RANDOMMEME/Assets/Scripts/DeleteMeme.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteMeme : MonoBehaviour
+{
+    public int id;
+    public string type;
+
+    public void OnDelete()
+    {
+        Destroy(gameObject);
+
+        if (type == "up")
+            MemLists.strings_up.RemoveAt(id);
+        else
+            MemLists.strings_down.RemoveAt(id);
+
+        SaveAndLoad.Save();
+    }
+}
---
+++ RANDOMMEME/Assets/Scripts/UpMemesList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpMemesList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < MemLists.strings_up.Count; i++)
+        {
+            var Panel = Instantiate(Resources.Load("Prefabs/MemePanel"), transform);
+            Transform child = transform.GetChild(i);
+            Transform text = child.GetChild(0);
+            Transform button = child.GetChild(1);
+
+            child.GetComponent<DeleteMeme>().id = i;
+            child.GetComponent<DeleteMeme>().type = "up";
+
+            text.GetComponent<Text>().text  = MemLists.strings_up[i];
+        }
+    }
+}
