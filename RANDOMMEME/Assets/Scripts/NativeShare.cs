87766f3fbe2459a615348fe72d0ab5450cf4bb94
Semen <35036074+Iridium6games@users.noreply.github.com>
2019-07-12 16:38:25 +0500

    +++FINAL

--- RANDOMMEME/Assets/Scripts/NativeShare.cs
+++ RANDOMMEME/Assets/Scripts/NativeShare.cs
@@ -1,76 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using VoxelBusters.NativePlugins;
 
 public class NativeShare : MonoBehaviour
 {
 
-    public string ScreenshotName = "screenshot.png"; // "имя" скриншота
-    public string url = "sdfdsf"; //url приложения (например на маркете)
-
-    /// <summary>
-    /// Метод для кнопки. 
-    /// </summary>
-    /// <param name="text">Текст которым делимся</param>
-    public void ShareScreenshotWithText(string text)
+    public void ShareScreenShotUsingShareSheet()
     {
-        ScreenCapture.CaptureScreenshot(ScreenshotName);
-        StartCoroutine(DelayedShare(text, url));
+        // Create share sheet
+        ShareSheet _shareSheet = new ShareSheet();
+        _shareSheet.Text = "https://play.google.com/store/apps/details?id=com.PickleHero.RandomMeme";
+
+        // Set this list if you want to exclude any service/application type. Else, ignore.
+        // _shareSheet.ExcludedShareOptions = m_excludedOptions;
+
+        // Attaching screenshot here
+        _shareSheet.AttachScreenShot();
+
+        // Show composer
+        NPBinding.UI.SetPopoverPointAtLastTouchPosition();
+        NPBinding.Sharing.ShowView(_shareSheet, FinishedSharing);
     }
 
-    /// <summary>
-    /// Небольшая пауза для скриншота и вызов метода "Поделиться"
-    /// </summary>
-    /// <param name="text">Текст которым делимся</param>
-    /// <param name="urlApp">Ссылка на приложение</param>
-    /// <returns></returns>
-    private IEnumerator DelayedShare(string text, string urlApp)
+    private void FinishedSharing(eShareResult _result)
     {
-        yield return new WaitForSeconds(0.25f);
-        string screenShotPath = Application.persistentDataPath + "/" + ScreenshotName;
-        Share(text, screenShotPath, urlApp);
-    }
-
-    //"Делимся"
-    private void Share(string shareText, string imagePath, string url, string subject = "Share")
-    {
-        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-
-        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-        /*AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + imagePath);
-        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-        intentObject.Call<AndroidJavaObject>("setType", "image/png");*/
-
-        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), shareText + "\n" + url);
-
-        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-
-        AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, subject);
-        currentActivity.Call("startActivity", jChooser);
-    }
-
-
-
-    public void OnShareClicked()
-    {
-
-        // Get the required Intent and UnityPlayer classes.
-        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-
-        // Construct the intent.
-        AndroidJavaObject intent = new AndroidJavaObject("android.content.Intent");
-        intent.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-        intent.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "Here's the text I want to share.");
-        intent.Call<AndroidJavaObject>("setType", "text/plain");
-
-        // Display the chooser.
-        AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
-        AndroidJavaObject chooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intent, "Share");
-        currentActivity.Call("startActivity", chooser);
-
+        throw new NotImplementedException();
     }
 
 }
