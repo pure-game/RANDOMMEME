b49bceba74bf7ebdbd9445382b21781b74a820e6
Semen <35036074+Iridium6games@users.noreply.github.com>
2019-07-12 17:00:26 +0500

    ll

--- RANDOMMEME/Assets/Scripts/NativeShare.cs
+++ RANDOMMEME/Assets/Scripts/NativeShare.cs
@@ -10,7 +10,7 @@ public class NativeShare : MonoBehaviour
     {
         // Create share sheet
         ShareSheet _shareSheet = new ShareSheet();
-        _shareSheet.Text = "https://play.google.com/store/apps/details?id=com.PickleHero.RandomMeme";
+        _shareSheet.Text = "Check this GENIUS game on GooglePlay: https://play.google.com/store/apps/details?id=com.PickleHero.RandomMeme";
 
         // Set this list if you want to exclude any service/application type. Else, ignore.
         // _shareSheet.ExcludedShareOptions = m_excludedOptions;
